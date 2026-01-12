da08a41acca34be4c24b5ce634634fb1c14712bf
Kishant151 <talaviya151@gmail.com>
2026-01-12 11:15:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,32 @@
-Versions.getVersionsInformation = ({ status, pattern, sort, pageNumber, limit, sortKey, customerIds, collection }) => {
-  return new Promise((resolve, reject) => {
-    const condition = { IsDeleted: false }
-    const searchCondition = {}
-    sort = sort === 'dsc' ? -1 : 1
-    sortKey = sortKey || 'VersionNumber'
-    const skips = limit * (pageNumber - 1)
-    if (customerIds && customerIds.length > 0) {
-      customerIds = customerIds.map(custId => {
-        return ObjectId.createFromHexString(custId)
-      })
-      Object.assign(condition, { CustomerID: { $in: customerIds } })
-    }
-    if (pattern) {
-      Object.assign(searchCondition, {
-        $or: [
-          { VersionNumber: new RegExp(pattern, 'i') },
-          { 'CustomerData.CustomerName': new RegExp(pattern, 'i') }
-        ]
-      })
-    }
-    if (status) {
-      status = status === true
-      Object.assign(condition, { IsActive: status })
-    }
-    const query = [
-      {
-        $match: condition
-      },
-      {
-        $lookup: {
-          from: 'Customers',
-          localField: 'CustomerID',
-          foreignField: '_id',
-          pipeline: [
-            { $project: { _id: 1, CustomerName: 1 } }
-          ],
-          as: 'CustomerData'
+module.exports = {
+  Query: {
+    async getAtriumBalance (_, {customerId, terminalId, accountMode, accountNumber, cardNumber, atriumEndpoint }, context) {
+      log.lambdaSetup(context, 'authProviders', 'getAtriumBalance')
+      try {
+        context.data.isKiosk  ? verifyKioskAndUserAccess(context, customerId) : verifyUserAccess(context, customerId)
+        const authData = await atriumAuthToken(config.region)
+        const token = await atriumGetToken(authData, atriumEndpoint)
+        console.log('authData', authData)
+        const getBalance = await atriumGetBalance(token, terminalId, accountMode, accountNumber, cardNumber, atriumEndpoint)
+        if (getBalance?.message?.toLowerCase() === 'approved'){
+          return {
+            Message: getBalance.message,
+            StatusCode: 200,
+            TransactionID: getBalance.txid,
+            RemainingAmount: getBalance?.amount?.remaining,
+            Currency: getBalance?.amount?.currency
+          }
+        } else {
+            log.info('transaction failed')
+            return {
+              Message: 'Failed to retrieve balance',
+              StatusCode: 400
+          }
         }
-      },
-      {
-        $unwind: {
-          path: '$CustomerData',
-          preserveNullAndEmptyArrays: true
-        }
-      },
-      {
-        $match: searchCondition
+      } catch (err) {
+        log.error(err)
+        throw new Error(err.message)
       }
-    ]
-    let totalQuery = query
-    totalQuery = totalQuery.concat({ $count: 'total' })
-    Promise.props({
-      version: pageNumber && limit
-        ? collection.aggregate(query, { collation: { locale: 'en' } })
-          .sort({ [sortKey]: sort })
-          .skip(skips)
-          .limit(limit).toArray()
-        : collection.aggregate(query, { collation: { locale: 'en' } })
-          .sort({ [sortKey]: sort }).toArray(),
-      total: collection.aggregate(totalQuery).toArray()
-    }).then(results => {
-      results.total = results.total[0] &&
-            results.total[0].total
-        ? results.total[0].total
-        : 0
-      resolve(results)
-    }).catch(err => {
-      console.log(err)
-      reject(err)
-    })
-  })
+    },
+  }
 }

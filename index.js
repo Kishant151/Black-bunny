1ae742266258a54045193a94d24d9a16913efaf2
Kishant151 <talaviya151@gmail.com>
2026-01-12 14:31:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,30 +2,21 @@ const crypto = require('crypto')
 const { EGHL_PAGE_TIMEOUT } = '780'
 const { basePath } = 'http://localhost:4000/public'
 
-const getHashKeyData = (argsData, encryptedData) => {
+const getHashKey = (argsData, encryptedData) => {
   const { CurrencyCode, Amount, ReturnURL, ApprovalURL, UnApprovalURL } = argsData
-  const { ServiceId } = encryptedData.eGHL
+  const { ServiceId, Config } = encryptedData.eGHL
 
-  /**
-   * eGHL Merchant Secret
-   * Used only for HMAC-based request signature.
-   * NOT a user password.
-   */
-  const merchantSecret = String(encryptedData.eGHL.Password)
-
-  const orderNumber = crypto.randomBytes(32).toString('hex').slice(0, 20)
-
+  const orderNumber = crypto.randomBytes(64).toString('hex').slice(0, 20)
+  const serviceID = ServiceId
   const paymentID = crypto
-    .randomBytes(32)
+    .randomBytes(64)
     .toString('hex')
     .slice(0, 20)
     .toUpperCase()
-
   const merchantReturnURL = ReturnURL
   const merchantApprovalURL = ApprovalURL
   const merchantUnApprovalURL = UnApprovalURL
   const merchantCallBackURL = `${basePath}/eghl/response`
-
   const amount = parseFloat(Amount).toFixed(2)
   const currencyCode = CurrencyCode
   const custIP = ''
@@ -34,11 +25,9 @@ const getHashKeyData = (argsData, encryptedData) => {
   const token = ''
   const recurringCriteria = ''
 
-  const hashPayload = `${merchantSecret}${ServiceId}${paymentID}${merchantReturnURL}${merchantApprovalURL}${merchantUnApprovalURL}${merchantCallBackURL}${amount}${currencyCode}${custIP}${pageTimeout}${cardNo}${token}${recurringCriteria}`
-
+  const hashKey = `${Config}${serviceID}${paymentID}${merchantReturnURL}${merchantApprovalURL}${merchantUnApprovalURL}${merchantCallBackURL}${amount}${currencyCode}${custIP}${pageTimeout}${cardNo}${token}${recurringCriteria}`
   return {
-    merchantSecret,
-    hashPayload,
+    hashKey,
     orderNumber,
     paymentID,
     pageTimeout,
@@ -50,25 +39,18 @@ const getHashKeyData = (argsData, encryptedData) => {
   }
 }
 
-const generateHash = (payload, secret) => {
-  return crypto
-    .createHmac('sha256', secret)
-    .update(payload, 'utf8')
-    .digest('hex')
+const getHash = (hashKey) => {
+  let hash = crypto.createHash('sha256')
+  data = hash.update(hashKey, 'utf8')
+  let hashValue = data.digest('hex')
+  return hashValue
 }
 
 const generateEghlHash = (argsData, encryptedData) => {
-  const eghlData = getHashKeyData(argsData, encryptedData)
-
-  const hash = generateHash(
-    eghlData.hashPayload,
-    eghlData.merchantSecret
-  )
-
-  delete eghlData.merchantSecret
-  delete eghlData.hashPayload
-
+  const eghlData = getHashKey(argsData, encryptedData)
+  const hash = getHash(eghlData.hashKey)
   return { hash, eghlData }
 }
 
 module.exports = generateEghlHash
+

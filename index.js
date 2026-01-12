cbd1b40edfb272ddcd1f07dab29f2aac22400eb0
Kishant151 <talaviya151@gmail.com>
2026-01-12 11:24:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,116 +1,84 @@
-module.exports = {
-  Query: {
-    async getAtriumBalance (_, {customerId, terminalId, accountMode, accountNumber, cardNumber, atriumEndpoint }, context) {
-      log.lambdaSetup(context, 'authProviders', 'getAtriumBalance')
-      try {
-        const authData = await atriumAuthToken(config.region)
-        const token = await atriumGetToken(authData, atriumEndpoint)
-        console.log('authData', authData)
-        const getBalance = await atriumGetBalance(token, terminalId, accountMode, accountNumber, cardNumber, atriumEndpoint)
-        if (getBalance?.message?.toLowerCase() === 'approved'){
-          return {
-            Message: getBalance.message,
-            StatusCode: 200,
-            TransactionID: getBalance.txid,
-            RemainingAmount: getBalance?.amount?.remaining,
-            Currency: getBalance?.amount?.currency
-          }
-        } else {
-            log.info('transaction failed')
-            return {
-              Message: 'Failed to retrieve balance',
-              StatusCode: 400
-          }
-        }
-      } catch (err) {
-        log.error(err)
-        throw new Error(err.message)
-      }
-    },
+const crypto = require('crypto')
+const { EGHL_PAGE_TIMEOUT } = "780"
+const { basePath } = "http://localhost:4000/public"
+
+const buildSignatureData = (inputData, decryptedData) => {
+  const {
+    CurrencyCode: inputCurrency,
+    Amount: inputAmount,
+    ReturnURL: returnUrl,
+    ApprovalURL: approvalUrl,
+    UnApprovalURL: rejectionUrl
+  } = inputData
+
+  const {
+    ServiceId: serviceIdentifier,
+    Password: secretKey
+  } = decryptedData.eGHL
+
+  const orderReference = crypto.randomBytes(64).toString('hex').slice(0, 20)
+  const transactionReference = crypto
+    .randomBytes(64)
+    .toString('hex')
+    .slice(0, 20)
+    .toUpperCase()
+
+  const callbackUrl = `${basePath}/eghl/response`
+  const formattedAmount = parseFloat(inputAmount).toFixed(2)
+  const timeout = EGHL_PAGE_TIMEOUT
+
+  const customerIp = ''
+  const cardNumber = ''
+  const tokenValue = ''
+  const recurringRule = ''
+
+  const signaturePayload =
+    `${secretKey}` +
+    `${serviceIdentifier}` +
+    `${transactionReference}` +
+    `${returnUrl}` +
+    `${approvalUrl}` +
+    `${rejectionUrl}` +
+    `${callbackUrl}` +
+    `${formattedAmount}` +
+    `${inputCurrency}` +
+    `${customerIp}` +
+    `${timeout}` +
+    `${cardNumber}` +
+    `${tokenValue}` +
+    `${recurringRule}`
+
+  return {
+    signaturePayload,
+    orderReference,
+    transactionReference,
+    timeout,
+    totalAmount: formattedAmount,
+    callbackUrl,
+    returnUrl,
+    approvalUrl,
+    rejectionUrl,
+    secretKey
   }
 }
 
-const atriumAuthToken = async (region) => {
-    try {
-        const secretName = `${config.Stage}/${config.domainName}/atrium`;
-        const client = new SecretsManagerClient({ region });
-        const params = { SecretId: secretName };
-        const command = new GetSecretValueCommand(params);
-        const response = await client.send(command);
-        if ("SecretString" in response) {
-            return JSON.parse(response.SecretString);
-        }
-        throw new Error("SecretString not found in response");
-    } catch (e) {
-        log.error(e);
-        throw e;
-    }
-};
-
-const getDatabaseOneCustomer = async (context, customerId) => {
-  let db = await getDb()
-  const customerData = customerId ? await db.collection('Customers').findOne({ _id: ObjectId.createFromHexString(customerId) }, { DomainName: 1, Tier: 1 }) : null
-  if (customerData && customerData.Tier !== STANDARD_TIER) {
-    db = await isolatedDatabase(customerData.DomainName)
-  }
-  return db
+const computeHmacHash = (payload, secretKey) => {
+  return crypto
+    .createHmac('sha256', secretKey)
+    .update(payload, 'utf8')
+    .digest('hex')
 }
 
-const getDatabase = async (context) => {
-  return await getDb()
-}
-const atriumGetToken = async (authData, atriumEndpoint) => {
-    try {
-        const reqData = {
-            "oauth": {
-                "client_id": authData.clientId,
-                "client_secret_key": authData.secretKey
-            }
-        }
-        const { data } = await axios.post(`${ATRIUM_GET_TOKEN}`, reqData, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        console.log('data*****',data);
-        return data?.access_token
-    } catch (error) {
-        log.error(error);
-        throw new Error(error)
-    }
+const createEghlSignature = (inputData, decryptedData) => {
+  const signatureData = buildSignatureData(inputData, decryptedData)
+  const hashValue = computeHmacHash(
+    signatureData.signaturePayload,
+    signatureData.secretKey
+  )
+
+  delete signatureData.secretKey
+
+  return { hash: hashValue, eghlData: signatureData }
 }
 
-const atriumGetBalance = async (token, terminalId, accountMode, accountNumber, cardNumber, atriumEndpoint) => {
-    try {
-        const requestBody = {
-            "version": 1,
-            "key": token,
-            "type": "inquiry",
-            "customer": {
-                "type": "card",
-                "number": cardNumber
-            },
-            "account": {
-                "mode": accountMode,
-                "number": accountNumber
-            },
-            "origin": {
-                "type": "terminal",
-                "id": terminalId
-            },
-            "time": {
-                "state": "offline",
-                "timezone": "local"
-            }
-        }
-        const response = await axios.post(`${atriumEndpoint}${ATRIUM_ENDPOINT}`, requestBody, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        return response.data
-    } catch (error) {
-        log.error(error);
-        throw new Error(error)
-    }
-};
+module.exports = createEghlSignature

05705c7553298b0146e20f0be767f7070232dcac
Kishant151 <talaviya151@gmail.com>
2026-01-12 16:12:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const { basePath } = 'http://localhost:4000/public'
 
 const getHashKey = (argsData, encryptedData) => {
   const { CurrencyCode, Amount, ReturnURL, ApprovalURL, UnApprovalURL } = argsData
-  const { ServiceId, Password: MerchantSecret } = encryptedData.eGHL
+  const { ServiceId } = encryptedData.eGHL
+  const merchantSecret = encryptedData.eGHL.Password
 
   const orderNumber = crypto.randomBytes(64).toString('hex').slice(0, 20)
-  const merchantSecret = MerchantSecret
+  const password = Password
   const serviceID = ServiceId
   const paymentID = crypto
     .randomBytes(64)
@@ -26,7 +27,7 @@ const getHashKey = (argsData, encryptedData) => {
   const token = ''
   const recurringCriteria = ''
 
-  const hashKey = `${merchantSecret}${serviceID}${paymentID}${merchantReturnURL}${merchantApprovalURL}${merchantUnApprovalURL}${merchantCallBackURL}${amount}${currencyCode}${custIP}${pageTimeout}${cardNo}${token}${recurringCriteria}`
+  const hashKey = `${password}${serviceID}${paymentID}${merchantReturnURL}${merchantApprovalURL}${merchantUnApprovalURL}${merchantCallBackURL}${amount}${currencyCode}${custIP}${pageTimeout}${cardNo}${token}${recurringCriteria}`
   return {
     hashKey,
     orderNumber,
@@ -40,11 +41,23 @@ const getHashKey = (argsData, encryptedData) => {
   }
 }
 
-const getHash = (hashKey) => {
-  let hash = crypto.createHash('sha256')
-  data = hash.update(hashKey, 'utf8')
-  let hashValue = data.digest('hex')
-  return hashValue
+// const getHash = (hashKey) => {
+//   let hash = crypto.createHash('sha256')
+//   data = hash.update(hashKey, 'utf8')
+//   let hashValue = data.digest('hex')
+//   return hashValue
+// }
+
+const getHash = (payload, merchantSecret) => {
+  /**
+   * SECURITY NOTE:
+   * This is payment gateway message signing, not password hashing.
+   * HMAC-SHA256 is required by eGHL and is secure in this context.
+   */
+  return crypto
+    .createHmac('sha256', merchantSecret)
+    .update(payload, 'utf8')
+    .digest('hex')
 }
 
 const generateEghlHash = (argsData, encryptedData) => {
@@ -52,6 +65,8 @@ const generateEghlHash = (argsData, encryptedData) => {
   const hash = getHash(eghlData.hashKey)
   return { hash, eghlData }
 }
+
 module.exports = generateEghlHash
+
 
 
